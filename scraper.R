c2c04e6da0189cb045f37891939ecf32d9bc16dc
h5n1mapping <h5n1mapping@gmail.com>
2025-03-04 04:00:04 +0000

    Updated scraper.R to use a function for the download logic

--- scraper.R
+++ scraper.R
@@ -1,11 +1,16 @@
 setwd("/cloud/project")
 
-url <- "https://www.aphis.usda.gov/livestock-poultry-disease/avian/avian-influenza/hpai-detections/mammals/sites/default/files/hpai-mammals.csv"
-dest_folder <- "data"
-dest_file <- file.path(dest_folder, "mammal.csv")
-
-if (!dir.exists(dest_folder)) {
-  dir.create(dest_folder)
+download_csv <- function(url, file_name) {
+  
+  dest_folder <- "data"
+  dest_file <- file.path(dest_folder, file_name)
+  
+  if (!dir.exists(dest_folder)) {
+    dir.create(dest_folder)
+  }
+  
+  download.file(url, dest_file, mode = "wb", quiet = TRUE)
 }
 
-download.file(url, dest_file, mode = "wb", quiet = TRUE)
+download_csv("https://www.aphis.usda.gov/livestock-poultry-disease/avian/avian-influenza/hpai-detections/mammals/sites/default/files/hpai-mammals.csv",
+             "mammal.csv")
